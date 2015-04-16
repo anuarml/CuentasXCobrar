9e62226348c46f270675d8e967fec6705510358f
YusselPP <yussel_paredes@hotmail.com>
2015-04-16 10:40:38 -0500

    Menu herramientas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,27 +22,44 @@
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 					<span class="sr-only">Toggle Navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
 				<a class="navbar-brand" href="#">Assis</a>
+				<a class="navbar-brand" href="{{ url('/') }}">Cobranza</a>
 			</div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Cobranza</a></li>
-				</ul>
-
+			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::check())
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
+						<li>
+							<a style="display:inline-block" href="javascript:newTransaction()">
+								<img height="30px" src="{{asset('img/new.png')}}">
+							</a>
+							<a style="display:inline-block" href="javascript:newTransaction()">
+								<img height="30px" src="{{asset('img/open.png')}}">
+							</a>
+							<a style="display:inline-block" href="javascript:newTransaction()">
+								<img height="30px" src="{{asset('img/save.png')}}">
+							</a>
+							<a style="display:inline-block" href="javascript:newTransaction()">
+								<img height="30px" src="{{asset('img/delete.png')}}">
+							</a>
+							<a style="display:inline-block" href="javascript:newTransaction()">
+								<img height="30px" src="{{asset('img/print.png')}}">
+							</a>
+							<a style="display:inline-block" href="javascript:newTransaction()">
+								<img height="30px" src="{{asset('img/affect.ico')}}">
+							</a>
+							<a style="display:inline-block" href="javascript:newTransaction()">
+								<img height="30px" src="{{asset('img/cancel.png')}}">
+							</a>
+							<a style="display:inline-block" href="{{ url('/auth/logout') }}">
+								<img height="30px" src="{{asset('img/logout.png')}}">
+							</a>
 						</li>
 					@endif
 				</ul>
