b76322844faaf0ef96d2d0ab936a5e57a6b7cc48
anuarml <anuar_ml93@hotmail.com>
2015-04-16 11:00:34 -0500

    Creacion de vista de datos generales

---
+++ public/js/datosGenerales.js
@@ -0,0 +1,10 @@
+/*$('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})*/ 
+$('.selectpicker').selectpicker();
+
+ $('.selectpicker').selectpicker({
+      style: 'btn-info',
+      size: 4
+  });
