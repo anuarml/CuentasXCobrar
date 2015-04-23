837f8a83e6abb02c90c4cbbbae48ed5942957ed9
anuarml <anuar_ml93@hotmail.com>
2015-04-23 09:28:43 -0500

    Finalizada vista de documentos

--- public/js/nuevoMovimiento.js
+++ public/js/nuevoMovimiento.js
@@ -89,21 +89,70 @@ $(".addnewrow").on("click", function(){
     createdocumentsTable(documents);
 });*/
 
-$("#documentsTable").bootstrapTable().on('load-success.bs.table', function (e, data) {	
+/*$("#documentsTable").bootstrapTable().on('load-success.bs.table', function (e, data) {	
 	$(".deleterow").on("click", function(){
 	var $killrow = $(this).parent('td').parent('tr');
 	    $killrow.addClass("danger");
 		$killrow.fadeOut(1000, function(){
 	    $(this).remove();
 	});});
-});
+});*/
+/*var documents = [];
+var numberOfDocuments = 0;
+function document(apply, consecutive, amount, difference, differencePercentage, concept, reference){
+	this.apply;
+	this.consecutive;
+	this.amount;
+	this.difference;
+	this.differencePercentage;
+	this.concept;
+	this.reference;
+}*/
 
 $(".addnewrow").on("click", function(){
-	$('#documentsTable tr:last').after("<tr><td style='text-align: center;'>100</td><td style='text-align: center;'>NEW</td><td style='text-align: center;'>NEW</td><td style='text-align: center;'>NEW</td><td style='text-align: center;'>NEW</td><td style='text-align: center;'>NEW</td><td style='text-align: center;'><div class='deleterows'><div class='glyphicon glyphicon-remove'></div></div></td><tr>");
-	$(".deleterows").on("click", function(){
+	
+	$('#documentsTable tbody').append("<tr><td style='text-align: center;' class='apply'></td><td style='text-align: center;' class='consecutive'></td><td style='text-align: center;' class='amount'></td><td style='text-align: center;' class='difference' ></td><td style='text-align: center;' class='differencePercentage'></td><td style='text-align: center;' class='concept'></td><td style='text-align: center;' class='reference'></td><td style='text-align: center;'><div class='deleterows'><div class='glyphicon glyphicon-remove'></div></div></td><td style='text-align: center;'><div class='editrow'><div class='glyphicon glyphicon-pencil'></div></div></td><td style='text-align: center;'><div class = 'calculator' align= 'center'><div class='fa fa-calculator'></div></div></td></tr>");
+	
+	$("#documentsTable tbody tr:last .deleterows").on("click", function(){
 		var $killrow = $(this).parent('td').parent('tr');
 		    $killrow.addClass("danger");
 			$killrow.fadeOut(1000, function(){
-		    $(this).remove();
-	});});}
+		    	$(this).remove();
+			});
+	});
+
+	$("#documentsTable tbody tr:last .apply").on("click", function(e){
+		$(e.target).append("<select class='form-control' id='apply'><option>a</option><option>b</option></select> ");
+		$("#apply").focus();
+	});
+
+	$("#documentsTable tbody tr:last .apply").on("focusout", function(e){
+		console.log($(this) + "gorgojo");
+		$("#apply option:selected").text();
+		$(this).empty();
+	});
+
+	$("#documentsTable tbody tr:last .consecutive").on("click", function(e){
+		/*$(e.target).append("<select class='form-control' id='apply'><option></option></select> ");
+		$("#apply").focus();*/
+		window.location("/cxc/documento/buscar")
+	});
+
+	
+
+	/*$(".apply").on("click", function(e){
+		//$(this).remove();
+		//console.log($(this));
+		$(e.target).append("<input type='text' id='txtApply'>")
+		//$("#txtApply").focus();
+	})*/
+
+	/*$(".apply").on("focusout", function(e){
+		//e.preventDefault();
+		console.log($(this) + "gorgojo");
+		$(this).empty();
+	})*/
+
+	;}
 );
+/*<select class='form-control'><option>Parangaricutirimicuaro</option></select>*/
