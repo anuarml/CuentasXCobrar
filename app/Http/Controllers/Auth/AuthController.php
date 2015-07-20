dbf4eca8eed956aa995547ee868c24f615a85ce6
YusselPP <yussel_paredes@hotmail.com>
2015-07-20 11:13:20 -0500

    Corregido error de redirección a Home
    Se sobrescribió el método redirectPath para enviar a la raiz / en lugar
    de a /home

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -105,4 +105,19 @@ class AuthController extends Controller {
 
 		return redirect()->intended($this->redirectPath());
 	}
+
+	/**
+	 * Get the post register / login redirect path.
+	 *
+	 * @return string
+	 */
+	public function redirectPath()
+	{
+		if (property_exists($this, 'redirectPath'))
+		{
+			return $this->redirectPath;
+		}
+
+		return property_exists($this, 'redirectTo') ? $this->redirectTo : '/';
+	}
 }
