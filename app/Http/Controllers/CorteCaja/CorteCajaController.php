fb253417d79e731d7362824aa337d00c6fa497f7
YusselPP <yussel_paredes@hotmail.com>
2015-10-13 18:56:59 -0500

    Agregado saldo de caja en el controlador
    Se agregó la consulta para obtener el saldo de la caja del usuario y
    mandarlo a la vista

--- app/Http/Controllers/CorteCaja/CorteCajaController.php
+++ app/Http/Controllers/CorteCaja/CorteCajaController.php
@@ -1,13 +1,10 @@
 <?php namespace App\Http\Controllers\CorteCaja;
 
-use App\Http\Requests;
+use App\CorteCaja\Dinero;
+use App\CorteCaja\DineroSaldo;
+use App\PaymentType;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Cxc;
-use App\CxcD;
-use App\CxcPending;
-use App\Shipment;
-use App\ShipmentMov;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CorteCajaController extends Controller {
 
@@ -15,138 +12,31 @@ class CorteCajaController extends Controller {
 		$this->middleware('auth');
 	}
 
-	/*
-		Se utiliza el agente que tenga configurado el usuario para obtener el embarque(Orden de Cobro) que tiene asignado.
-		
-		Para ver las facturas o documentos que debe cobrar el usuario:
-		  Estas se obtienen mediante el movimiento 'Orden Cobro' (Embarques) en estatus PENDIENTE
-		  que tenga asignada el agente configurado en el usuario.
 
-		Para ver las facturas o documentos que ha cobrado el usuario:
-		  Estas se obtienen de los movimiento 'Cobro' (CXC) en estatus CONCLUIDO que haya realizado
-		  el usuario.
-	*/
-
-	/*public function getEmbarques(){
+	public function showCorteCaja(){
 
 		$user = \Auth::user();
+		$company = $user->getSelectedCompany();
+		$moneyAccount = $user->account; // VALIDAR LA CAJA AL INICIAR SESIÓN......
 
-		$chargeOrderCompare = collect([]);
-
-		// Se obtiene el ID de la orden de cobro asignada al usuario.
-		$chargeOrderID = Shipment::getChargeOrdersID();
-		// Se obtienen los DOCUMENTOS asignados al usuario en la orden de cobro.
-		$assignedDocuments = ShipmentMov::getAsignedDocuments($chargeOrderID);
-		// Se obtienen los DOCUMENTOS que ha cobrado el usuario durante la ruta de cobro asignada.
-		$chargedDocuments = Cxc::getChargedDocuments($chargeOrderID);
-		// Se obtienen los ANTICIPOS que ha cobrado el usuario durante la ruta de cobro asignada.
-		$chargedAdvances = Cxc::getChargedAdvances($chargeOrderID);
+		$currency = config('cxc.default_currency');
 
 
-		foreach ($assignedDocuments as &$assignedDocument) {
-
-			$mov = trim($assignedDocument->Mov);
-			$movID = trim($assignedDocument->MovID);
-
-			$documentOrigin = Cxc::where('Mov',$mov)->where('MovID',$movID)->where('Empresa',$user->getSelectedCompany())->first(['Saldo','Cliente']);
-
-			$document = new \stdClass;
-
-			if($documentOrigin){
-				$document->balance = $documentOrigin->balance;
-			} else{
-				$document->balance = 0;
-			}
-			$document->client = $assignedDocument->client;
-			$document->assigned = 1;
-			$document->charged = false;
-			$document->cashed = 0;
-			$document->mov = $mov;
-			$document->movID = $movID;
-			
-			foreach ($chargedDocuments as &$chargedDocument) {
-
-				$apply = trim($chargedDocument->apply);
-				$applyID = trim($chargedDocument->apply_id);
-
-				if( $mov == $apply && $movID == $applyID ){
-					$document->charged = true;
-					$document->cashed += $chargedDocument->amount;
-					$chargedDocument->assigned = 1;
-				}
-			}
-
-			$chargeOrderCompare->push($document);
+		// Se obtiene el saldo de la caja del usuario.
+		$dineroSaldo = DineroSaldo::getDineroSaldo($company, $currency, $moneyAccount)
+							->get([DineroSaldo::COLUMN_NAME_SALDO])
+							->first();
+		$saldo = 0;
+		if($dineroSaldo){
+			$saldo = $dineroSaldo->Saldo;
 		}
 
+		$paymentTypeList = [];//PaymentType::Web()->select('FormaPago')->get()->toArray();
 
-		// Se obtienen los documentos que se cobraron y no estaban asignados.
-		$unassignedChargedDocuments = $chargedDocuments->where('assigned',null);
+		$din = new Dinero;
 
-		foreach ($unassignedChargedDocuments as &$unassignedChargedDocument) {
-			
-			$apply = trim($unassignedChargedDocument->apply);
-			$apply_id = trim($unassignedChargedDocument->apply_id);
-
-			$coincidence = $chargeOrderCompare->where('mov',$apply)->where('movID',$apply_id);
-
-			if( $coincidence->isEmpty() ){
-
-				$documentOrigin = Cxc::where('Mov',$apply)->where('MovID',$apply_id)->where('Empresa',$user->getSelectedCompany())->first(['Saldo','Cliente']);
-
-				$document = new \stdClass;
-				if($documentOrigin){
-					$document->balance = $documentOrigin->balance;
-					$document->client = $documentOrigin->client_id;
-
-					if($apply == Cxc::getAdvanceName()){
-						$document->assigned = 2;
-					}
-				} else{
-					$document->balance = 0;
-					$document->client = null;
-					$document->assigned = 0;
-				}
-				
-				$document->charged = true;
-				$document->cashed = $unassignedChargedDocument->amount;
-				//$document->balance = $documentOrigin->balance;
-				//$document->client = $documentOrigin->client_id;
-				$document->mov = $apply;
-				$document->movID = $apply_id;
-
-				$chargeOrderCompare->push($document);
-			}
-			else{
-				$coincidence->first()->cashed += $unassignedChargedDocument->amount;
-			}
-		}
-
-		foreach ($chargedAdvances as $advance) {
-			$document = new \stdClass;
-
-			$document->balance = $advance->balance;
-			$document->client = $advance->client_id;
-			$document->assigned = 2;
-			$document->charged = true;
-			$document->cashed = $advance->total_amount;
-			$document->mov = $advance->Mov;
-			$document->movID = $advance->MovID;
-
-			$chargeOrderCompare->push($document);
-		}
-
-		//$chargeOrderCompare->sum('cashed');
-
-		//dd([$assignedDocuments,$unassignedChargedDocuments]);
-		//dd($chargeOrderCompare);
-		return response()->json($chargeOrderCompare);
-	}*/
-
-	public static function showCorteCaja(){
-		
-		$dataURL = 'embarques/embarques';
-		
-		return view('corteDeCaja.corteCaja', compact('dataURL'));
+		return view('corteDeCaja.corteCaja', compact('saldo','paymentTypeList'));
 	}
+
+
 }
--- config/cxc.php
+++ config/cxc.php
@@ -3,4 +3,5 @@
 return [
 
 	'cxc_default_office' => '0',
+	'default_currency' => 'Pesos',
 ];
